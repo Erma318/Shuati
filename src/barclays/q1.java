ff5d83a0e56386974f160b7db7b952e526e77023
Erma318 <fengyz318@gmail.com>
2018-08-30 17:23:58 -0500

    update

---
+++ src/barclays/q1.java
@@ -0,0 +1,45 @@
+package barclays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class q1 {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
+        BufferedReader in = new BufferedReader(reader);
+        String line;
+        while ((line = in.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+    public boolean validSquare(String line) {
+        String[] input = line.split(", ");
+        String[] p1s = input[0].split(",");
+        int[] p1 = {Character.getNumericValue(p1s[0].charAt(1)),Character.getNumericValue(p1s[1].charAt(0))};
+        String[] p2s = input[1].split(",");
+        int[] p2 = {Character.getNumericValue(p2s[0].charAt(1)),Character.getNumericValue(p2s[1].charAt(0))};
+        String[] p3s = input[2].split(",");
+        int[] p3 = {Character.getNumericValue(p3s[0].charAt(1)),Character.getNumericValue(p3s[1].charAt(0))};
+        String[] p4s = input[3].split(",");
+        int[] p4 = {Character.getNumericValue(p4s[0].charAt(1)),Character.getNumericValue(p4s[1].charAt(0))};
+        int p[][]={{p1[0],p1[1]},{p2[0],p2[1]},{p3[0],p3[1]},{p4[0],p4[1]}};
+        int [] len;
+        int cnt = 0;
+        len=new int[6];
+        for(int i = 0;i < 4;i++){
+            for(int j = i + 1;j < 4;j++){
+                len[cnt++]=(p[i][0] - p[j][0])*(p[i][0] - p[j][0])+(p[i][1] - p[j][1])*(p[i][1] - p[j][1]);
+            }
+        }
+        Arrays.sort(len);
+        if(len[0] == len[3] && len[4] == len[5] && len[4] > len[0]){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
--- src/drawBridge/stringValid.java
+++ src/drawBridge/stringValid.java
@@ -1,4 +1,69 @@
 package drawBridge;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class stringValid {
+    public String stringValid(String[] input) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < input.length; i++) {
+            if (valid(input[i])) {
+                res.append("Valid");
+                if (i != input.length - 1) {
+                    res.append("\n");
+                }
+            } else {
+                res.append("Invalid");
+                if (i != input.length - 1) {
+                    res.append("\n");
+                }
+            }
+        }
+        return res.toString();
+    }
+    private boolean valid(String input) {
+        String[] in = input.split(",");
+        if (in.length != 2) return false;
+        char[] left = in[0].toCharArray();
+        char[] right = in[1].toCharArray();
+        if (left[0] != '(' || right[right.length - 1] != ')') return false;
+        if (left[1] == ' ' || left[left.length - 1] > 57 || left[left.length - 1] < 48|| right[0] != ' ' || right[right.length - 2] > 57 || right[right.length - 2] < 48) return false;
+        StringBuilder sbl = new StringBuilder();
+        StringBuilder sbr = new StringBuilder();
+        for (int i = 1; i < left.length; i++) {
+            if ( left[i] > 57 || left[i] < 48) {
+                if (i == 1 && (left[i] == '+' || left[i] == '-')) {
+
+                } else {
+                    return false;
+                }
+            }
+            sbl.append(left[i]);
+        }
+        double X = Double.parseDouble(sbl.toString());
+        for (int i = 1; i < right.length - 1; i++) {
+            if ( right[i] > 57 || right[i] < 48) {
+                if (i == 1 && (right[i] == '+' || right[i] == '-')) {
+
+                } else {
+                    return false;
+                }
+            }
+            sbr.append(right[i]);
+        }
+        double Y = Double.parseDouble(sbr.toString());
+        if (X < -90 || X > 90 || Y < -180 || Y > 180) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        stringValid here = new stringValid();
+        String[] input = {"(75, 180)",
+                "(+90, +180.2)",
+                "(90, 180)"};
+        System.out.println(here.stringValid(input));
+    }
+
 }
