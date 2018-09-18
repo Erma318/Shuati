ce3bd895030073d73d0ab4657c0030605c70e90c
Erma318 <fengyz318@gmail.com>
2018-09-17 19:08:02 -0500

    update

--- src/CodeReview.java
+++ src/CodeReview.java
@@ -1,6 +1,17 @@
+import com.sun.org.apache.bcel.internal.classfile.Code;
+import sun.reflect.generics.tree.Tree;
+
 import java.util.*;
 
+
+
+
 public class CodeReview {
-
-
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("hundred");
+        sb.append(" ");
+        sb.append("one");
+        System.out.println(sb.reverse().toString());
+    }
 }
